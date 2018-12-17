75ead6b9d6b4a405d2bb952e427265506cae747b
Nicola <nicolawongwaiyee@gmail.com>
2018-12-17 14:40:51 +0100

    seperate fasta class

---
+++ bioinfoFasta.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+class Fasta(object):
+    def __init__(self, path: str):
+        self.path = path
+        seq = {}
+        with open(path, "r") as f:
+            for line in f:
+                if line.startswith(">"):
+                    seqid = line.rstrip().replace(">", "")
+                    seq[seqid]=""
+                else:
+                    seq[seqid] = seq[seqid] + line.rstrip()
+        self.seqDict = seq
+
+    def labelSeq(self, m)->None:
+        for k, v in self.seqDict.items():
+            if m.get(k):
+                self.seqDict[k + "|" + m[k]] = v
+                del self.seqDict[k]
+
+    def seqlen(self)->dict:
+        seql = {}
+        for k, v in self.seqDict.items():
+            seql[k] = len(v)
+        return seql
+
+    def parseIdentifier(self)->dict:
+        d = {}
+        seqName = [*self.seqDict]
+        for i in seqName:
+            a = seqName.split("|")
+            d[a] = []
+            for j in range(1,len(a)-1):
+                d[a].append(j)
+        return d
